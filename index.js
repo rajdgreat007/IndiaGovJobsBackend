7d99a92875e668c3ccf8be0555abe252b4a7d0bd
Raj Gupta <raj.gupta@walmartlabs.com>
2018-09-22 21:15:39 +0530

    added basic server setup

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/indiagovjobs");
+var deviceDetailsSchema = new mongoose.Schema({
+    osType: String,
+    deviceToken: String
+});
+
+var Device = mongoose.model("Device", deviceDetailsSchema);
+
+app.get('/', (req, res) => {
+    res.send('server is up!');
+});
+
+app.post('/addDevice', (req, res) => {
+    var device = new Device(req.body);
+    device.save()
+        .then(item => {
+            res.send("device saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save device to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log('server listening on port : ' + port);
+});

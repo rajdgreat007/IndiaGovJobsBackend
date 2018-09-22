358c7f8ece876b056e696e87582c196579b629c9
Raj Gupta <raj.gupta@walmartlabs.com>
2018-09-22 23:13:57 +0530

    added port using env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
